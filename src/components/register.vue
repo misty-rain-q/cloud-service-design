b062cc7e850e5e17e9f31cf834d74678ee6408bc
Cishenn Lee <740804059@qq.com>
2020-07-04 16:41:03 +0800

    登陆和注册改的UI象样了.. 但是代码还有很多w问题..待解决

--- src/components/register.vue
+++ src/components/register.vue
@@ -10,8 +10,17 @@
                 <input placeholder="请输入邮箱或者手机号码" type = "text"></div>
                 <div class = "item"><label for = "">密码</label>
                 <input placeholder="请输入密码" type = "password"></div>
-                <div class = "item"><label for = "">验证码</label>
-                <input placeholder="请输入右侧图形验证码" type="text"></div>
+
+                <div class = "verItem"><label for = "">验证码</label>
+                <input id="verInput" placeholder="请输入右侧图形验证码" type="text">
+                <canvas id="canvas" 
+                        width="100px" 
+                        height="40px" 
+                        @click.prevent="drawPic">
+                        </canvas>
+                </div>
+                
+                
                 <el-row>
                    <el-button class="item btn" type="warning"
                    @click="register">注册</el-button>
@@ -35,15 +44,82 @@
           }
       },
       methods: {
-          // 登陆
-          register(){
-              this.$router.replace('/login');
-              this.$message({
-                  message: '注册成功',
-                  type: 'success'
-              })
-          }
+        // 登陆
+        register(){
+            this.$message({
+                message: '注册成功',
+                type: 'success'
+            });
+            this.$router.replace('/login');
+        },
+          // 登陆可能会用到验证码
+        randomNum(min, max) {
+          return Math.floor(Math.random() * (max - min) + min);
+        },
+        //生成随机颜色RGB分量
+        randomColor(min, max) {
+            var _r = this.randomNum(min, max);
+            var _g = this.randomNum(min, max);
+            var _b = this.randomNum(min, max);
+            return "rgb(" + _r + "," + _g + "," + _b + ")";
+        },
+        drawPic() {
+        //获取到元素canvas
+        var $canvas = document.getElementById("canvas");
+        var _str = "23456789abcdefghjkmnpqrstuvwxyz"; //设置随机数库
+        var _picTxt = ""; //随机数
+        var _num = 4; //4个随机数字
+        var _width = $canvas.width;
+        var _height = $canvas.height;
+        var ctx = $canvas.getContext("2d"); //获取 context 对象
+        ctx.textBaseline = "bottom"; //文字上下对齐方式--底部对齐
+        ctx.fillStyle = this.randomColor(180, 240); //填充画布颜色
+        ctx.fillRect(0, 0, _width, _height); //填充矩形--画画
+        for (var i = 0; i < _num; i++) {
+            var x = ((_width - 10) / _num) * i + 10;
+            var y = this.randomNum(_height / 2, _height);
+            var deg = this.randomNum(-45, 45);
+            var txt = _str[this.randomNum(0, _str.length)];
+            _picTxt += txt; //获取一个随机数
+            ctx.fillStyle = this.randomColor(10, 100); //填充随机颜色
+            ctx.font = this.randomNum(16, 40) + "px SimHei"; //设置随机数大小，字体为SimHei
+            ctx.translate(x, y); //将当前xy坐标作为原始坐标
+            ctx.rotate((deg * Math.PI) / 180); //旋转随机角度
+            ctx.fillText(txt, 0, 0); //绘制填色的文本
+            ctx.rotate((-deg * Math.PI) / 180);
+            ctx.translate(-x, -y);
+        }
+        for (var i = 0; i < _num; i++) {
+            //定义笔触颜色
+            ctx.strokeStyle = this.randomColor(90, 180);
+            ctx.beginPath();
+            //随机划线--4条路径
+            ctx.moveTo(this.randomNum(0, _width), this.randomNum(0, _height));
+            ctx.lineTo(this.randomNum(0, _width), this.randomNum(0, _height));
+            ctx.stroke();
+        }
+        for (var i = 0; i < _num * 10; i++) {
+            ctx.fillStyle = this.randomColor(0, 255);
+            ctx.beginPath();
+            //随机画原，填充颜色
+            ctx.arc(
+            this.randomNum(0, _width),
+            this.randomNum(0, _height),
+            1,
+            0,
+            2 * Math.PI
+            );
+            ctx.fill();
+        }
+
+        this.identifyCode = _picTxt;
+        }
     },
+    created() {
+        this.$nextTick(() => {
+           this.drawPic();
+        });
+    }
   }
 </script>
 
@@ -74,47 +150,80 @@
     border-radius: 5px;
     height:350px;
 }
-  h3{
-    text-align: left;
-    font-family: PingFangSC-Semibold,"PingFang SC Semibold", "PingFang SC", sans-serif;
-    font-size: 32px;
-    font-weight: 650px;
-    margin-bottom: 47px;
-  }
-  .item{
-    width: 358px;
-    height:48px;
-    background-color: #fff;
-    margin-bottom: 16px;
-    padding: 12px;
-    box-sizing: border-box;
-    border-radius: 2px;
-  }
-  .item label{
-    font-size: 14px;
-    font-weight: 400;
-    color:#333333;
-    line-height: normal;
-    font-feature-settings: "kern";
-  }
-  .item input{
-    width: 280px;
-    height: 23px;
-    padding: 3px 2px 3px 2px;
-    border-radius: 0px;
-    border-style: none;
-    outline-style: none;
-   }
-    .item.btn{
-        background-color: rgb(255,145,56);
-        text-align: center;
-        margin-top: 20px;
-     }
-    .login{
-         color: white;
-         margin-top: 15px;
-         font-size: 13px;
-         height: 40px;
-         text-align: center;
-      }
+h3{
+  text-align: left;
+  font-family: PingFangSC-Semibold,"PingFang SC Semibold", "PingFang SC", sans-serif;
+  font-size: 32px;
+  font-weight: 650px;
+  margin-bottom: 47px;
+}
+.item{
+  width: 358px;
+  height:48px;
+  background-color: #fff;
+  margin-bottom: 16px;
+  padding: 12px;
+  box-sizing: border-box;
+  border-radius: 2px;
+}
+.verItem{
+  width: 358px;
+  height:48px;
+  background-color: #fff;
+  margin-bottom: 16px;
+  padding: 12px;
+  box-sizing: border-box;
+  border-radius: 2px;
+  display: flex;
+  flex-direction: row;
+}
+.verItem label{
+  font-size: 14px;
+  font-weight: 400;
+  color:#333333;
+  line-height: normal;
+  font-feature-settings: "kern";
+  padding-top: 6px;
+}
+.verItem input{
+  width: 150px;
+  height: 23px;
+  padding: 3px 2px 3px 2px;
+  border-radius: 0px;
+  border-style: none;
+  outline-style: none;
+}
+.item label{
+  font-size: 14px;
+  font-weight: 400;
+  color:#333333;
+  line-height: normal;
+  font-feature-settings: "kern";
+}
+.item input{
+  width: 280px;
+  height: 23px;
+  padding: 3px 2px 3px 2px;
+  border-radius: 0px;
+  border-style: none;
+  outline-style: none;
+}
+.item.btn{
+  background-color: rgb(255,145,56);
+  text-align: center;
+  margin-top: 20px;
+}
+.login{
+  color: white;
+  margin-top: 15px;
+  font-size: 13px;
+  height: 40px;
+  text-align: center;
+}
+#canvas{
+  display: inline;
+  cursor: pointer;
+  margin-left: 30px;
+}
+
 </style>
